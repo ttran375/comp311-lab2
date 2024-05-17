1f7bc2e4db65fbbb89a9335867378e67e687dc08
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-17 16:09:51 +0000

    Fri, May 17, 2024, 4:09 PM +00:00

--- com/cc/airline/utilities/Manifest.java
+++ com/cc/airline/utilities/Manifest.java
@@ -2,10 +2,6 @@ package com.cc.airline.utilities;
 
 import java.util.ArrayList;
 
-import com.cc.airline.ticketing.Seat;
-import com.cc.airline.ticketing.SeatingClass;
-import com.cc.airline.ticketing.SeatingPlan;
-
 public class Manifest {
 	SeatingPlan plan;
 
@@ -16,11 +12,11 @@ public class Manifest {
 	private void printOut() {
 		ArrayList<Seat> seats = plan.getSeats();
 		System.out.println("Flight manifest");
-		for (Seat s: seats) {
+		for (Seat s : seats) {
 			System.out.println(s);
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		System.out.println("Flight open for ticket sales.");
 		SeatingPlan plan = new SeatingPlan();
@@ -28,12 +24,12 @@ public class Manifest {
 		SeatReserver reserver = plan.getSeatReserver();
 		int seatsSold = 0;
 		UserPrompter prompter = new UserPrompter("Do you want to purchace a ticket?");
-		while ( prompter.getYesNoAnswer() && seatsSold < plan.getTotalSeats() ) {
-			for ( int c = 0; c < SeatingClass.values().length; c++ ) {
+		while (prompter.getYesNoAnswer() && seatsSold < plan.getTotalSeats()) {
+			for (int c = 0; c < SeatingClass.values().length; c++) {
 				SeatingClass sClass = SeatingClass.values()[c];
-				if ( sClass.getNumSeatsSold() < sClass.getNumSeats() ){
-					UserPrompter prompt2 = new UserPrompter(" Do you want " + sClass + " class? " );
-					if ( prompt2.getYesNoAnswer()) {
+				if (sClass.getNumSeatsSold() < sClass.getNumSeats()) {
+					UserPrompter prompt2 = new UserPrompter(" Do you want " + sClass + " class? ");
+					if (prompt2.getYesNoAnswer()) {
 						reserver.sellTicket(sClass);
 						break;
 					}
